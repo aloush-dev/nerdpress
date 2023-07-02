25341f45668d3cb251f414bc248181ad4bc976d0
Ali <itsaliabdallah@gmail.com>
2023-07-02 22:36:56 +0100

    update service component display

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -7,11 +7,10 @@ export function Service({
   price: number;
   description: string;
 }) {
-
   return (
-    <div className="w-80 bg-[#83948e] p-6 text-[#fbf2e4]">
-      <h3 className="pb-2 font-bold" >{title.toUpperCase()}</h3>
-      <p className="pb-2 text-2xl " >£{price}</p>
+    <div className="flex h-full flex-col bg-[#83948e] p-6 text-[#fbf2e4]">
+      <h3 className="pb-2 font-bold">{title.toUpperCase()}</h3>
+      <p className="pb-2 text-2xl ">£{price}</p>
       <p>{description}</p>
     </div>
   );
--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -8,7 +8,7 @@ export default function Services() {
   return (
     <>
       <Heading text="Services" />
-      <div className="flex flex-wrap items-center justify-center gap-10 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-4">
         {data?.map((service) => {
           return (
             <Service

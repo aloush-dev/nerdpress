9a97900f4d57fd7aa15de9b5568d9a07ecd72dda
Ali <itsaliabdallah@gmail.com>
2023-07-28 23:30:15 +0100

    small faq style changes

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,8 +1,8 @@
 export function Faq({ data }: { data: Faq }) {
   return (
-    <li className="m-4 bg-theme-header p-2">
-      <div className="text-xl font-bold">{data.question}</div>
-      <div>{data.answer}</div>
+    <li className="m-4 bg-theme-header p-4 ">
+      <div className="pb-4 text-xl font-bold">{data.question}</div>
+      <div className="">{data.answer}</div>
     </li>
   );
 }
